31079b62ae3761c52810821a8a59b005622a36b8
swati2904 <swati.lpu17@gmail.com>
2020-06-06 02:59:37 +0530

    FSJ0520A108

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import model.Blog;
 import model.User;
 import service.CRUDOperations;
-import service.ExcelFileStorage;
+//import service.ExcelFileStorage;
 import utility.CheckBlogPost;
 
 
@@ -46,6 +46,7 @@ public class BlogController extends HttpServlet {
 		LocalDate postedOn = LocalDate.now();
 		System.out.println(title);
 		System.out.println(description);
+		System.out.println(postedOn);
 		
 		User user = null;
 		Blog blog=new Blog(title,description,postedOn);
@@ -64,8 +65,8 @@ public class BlogController extends HttpServlet {
 
 		if(check) {
 			request.setAttribute("listBlog", listblog);
-//			request.setAttribute("blog", blog);
-//			request.setAttribute("user",user);
+		request.setAttribute("blog", blog);
+		request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -4,26 +4,44 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import model.Blog;
 
-public class CRUDOperations {
+public class CRUDOperations
+{
 	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
 	
-	public List<Blog> createBlog(Blog blog) throws IOException {
+	public List<Blog> createBlog(Blog blog) throws IOException 
+	{
 		list.add(blog);
+		//System.out.println("after  adding"+list);
 		return list;
 	}
 	
-	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
+	public List<Blog> viewBlog(Blog blog) throws IOException
+	{
+		//System.out.println(list);
+		
+		return list;
 		
 	}
-	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
+	public List<Blog> updateBlog(Blog blog) throws IOException
+	{
+		list.set(list.indexOf(blog), blog);
+		
+		return list;
 	
 	}
-	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
+	public List<Blog> deleteBlog(Blog blog) throws IOException
+	{
+	      list.remove(blog);
+	      System.out.println("after remove "+list);
+		return list;
 		
 	}
+	
+	
+	
+	
 }
